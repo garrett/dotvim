97618ccd556da5ee57c98b381964c4b99bc920c4
Garrett LeSage <garrett@lesage.us>
2013-01-25 12:50:52 +0100

    handle fresh repositories (existing ones will disregard it)

--- update.sh
+++ update.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+# Check out submodules; useful for a first-time repo
+git submodule init && git submodule update
+
+# Update submodules to their latest versions
 git submodule foreach git pull origin master
